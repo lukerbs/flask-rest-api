c07179d470b079183d61683e75ea7ca6552d8dcc
Luke Kerbs <luke@Lukes-MBP-3.attlocal.net>
2022-03-30 16:25:42 -0700

    fixed http request bug

--- test_api.py
+++ test_api.py
@@ -1,23 +1,27 @@
 import requests
+import json
 import time
 
 BASE = 'http://127.0.0.1:8003/api/'
 
 data = [
-	{"name":"Luke", "age": 26, "location":"San Juan Bautista"},
+	{"name":"Luke", "age": 26, "gender":"Male", "location":"San Juan Bautista"},
 	{"name":"Robert", "location":"Gilroy"},
 	{"name":"Hannah", "location":"Paicines"},
 	{"name":"Bella", "age": 25, "location":"San Juan Bautista"}
 	]
 
+for i in range(len(data)):
+	data[i] = json.dumps(data[i], indent = 4) 
 
+headers = {"Content-Type": "application/json"}
 
 # test put request
 # send data to server
 print('\n')
 print('PUT REQUEST - Adding Data to Database')
 for i in range(len(data)):
-	response = requests.put(BASE + str(i), data[i])
+	response = requests.put(BASE + str(i), data=data[i], headers=headers)
 	print(response.json(), '\n')
 	time.sleep(.1)
 print('\n\n')
